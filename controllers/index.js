c260925a7914aaff783fd6ea2830797f9740fae7
Ruben <[connor.j.schulz@gmail.com]>
2022-06-30 19:49:15 -0400

    refractored files, and created live homepage

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 
 //put requires for all files here
 const apiRoutes = require('./apis');
+const homeRoutes = require('./homepage-routes');
 
 //put router.use for all files with paths
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+
 
 router.use((req, res) => {
     res.status(404).end();

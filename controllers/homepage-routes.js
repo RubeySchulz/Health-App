89180629acd8b0153c2559fb37188689485e09f6
Ruben <[connor.j.schulz@gmail.com]>
2022-07-17 17:23:21 -0400

    added dashboard route

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -4,14 +4,26 @@ const {Day, Nutrients, User} = require('../models');
 
 router.get('/', (req, res) => {
     res.render('homepage', {
-        loggedIn: req.session.loggedIn
+        loggedIn: req.session.loggedIn,
+        oldNavbar: true,
+        dashboard: false
     });
 });
 
 router.get('/signup', (req, res) => {
     res.render('signup-form', {
-        loggedIn: req.session.loggedIn
+        loggedIn: req.session.loggedIn,
+        oldNavbar: true,
+        dashboard: false
     });
-})
+});
+
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard', {
+        loggedIn: req.session.loggedIn,
+        oldNavbar: false,
+        dashboard: true
+    });
+});
 
 module.exports = router;

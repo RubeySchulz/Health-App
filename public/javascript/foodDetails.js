58b884d02322b9575dbf189b9af78369cdf15dc5
JoeyByrd <joeybyrd9999@gmail.com>
2022-07-21 11:21:23 -0700

    updated java

--- public/javascript/foodDetails.js
+++ public/javascript/foodDetails.js
@@ -11,7 +11,7 @@ var getFoodId = function() {
 
     var apiId = "app_id=128267bc"
     var apiKey = "app_key=331d3e04b2f9fcb5074581b87838db5b"
-    var foodUrl = "https://api.edamam.com/api/food-database/v2/parser?" + apiId + "&" + apiKey + "&ingr=" + foodForm + "&nutrition-type=logging";
+    var foodUrl = "https://api.edamam.com/api/food-database/v2/parser?" + apiId + "&" + apiKey + "&ingr=" + food.value + "&nutrition-type=logging";
 
     if(foodId) {
         fetch(foodUrl).then(function(response) {
@@ -28,8 +28,10 @@ var getFoodId = function() {
 var displayFoodInfo = function(food) {
     foodImg.setAttribute('src', food.image.medium)
     foodName.textContent = food.label;
-    foodCalories.textContent = food.calories;
-    
+    foodCalories.textContent = food.ENERC_KCAL;
+    foodProtien.textContent = food;
+    foodFat.textContent = food;
+    foodCarbs.textContent = food;
 }
 
 getFoodId();
